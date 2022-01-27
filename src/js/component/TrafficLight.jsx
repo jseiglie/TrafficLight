4682e27e7b5d64bdf5f3355de414ecb30d1250ad
jseiglie <javierseiglie@gmail.com>
2022-01-27 08:47:34 +0000

    components created

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../style/TrafficLight.css";
+import Home from "../views/home";
+
+const TrafficLight = () => {
+	return (
+		<>
+			<div className="row">
+				<div className="container myContainer">
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Amet, quas quo. Quam vero tenetur placeat explicabo
+						fuga, impedit enim labore repellat itaque sequi maxime
+						ducimus dolorem consequuntur atque nemo et.
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default TrafficLight;
---
+++ src/js/views/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TrafficLight from "../component/TrafficLight.jsx";
+//include images into your bundle
+
+//create your first component
+const Home = () => {
+	return (
+		<>
+			<div>
+				<TrafficLight />
+			</div>
+		</>
+	);
+};
+
+export default Home;
